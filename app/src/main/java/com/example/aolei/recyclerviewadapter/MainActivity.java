10fff8fb5c291711896b346fb16d45db91e086c8
aolei <807648567@qq.com>
2016-07-21 10:46:25 +0800

    add new project about RecyclerViewCommonAdapter

---
+++ app/src/main/java/com/example/aolei/recyclerviewadapter/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.aolei.recyclerviewadapter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private List<String> mDates = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView)findViewById(R.id.rv);
+        for (int i = 0; i < 10; i++) {
+            mDates.add(i+"");
+        }
+        Log.d("mDate",mDates.toString());
+        //获得线性布局的管理器
+        mRecyclerView = (RecyclerView)findViewById(R.id.rv);
+        final RecyclerViewvViewCommonAdapter commonAdapter = new RecyclerViewvViewCommonAdapter<String>(MainActivity.this,R.layout.recyclertest,mDates) {
+
+            @Override
+            public void convert(final RecyclerViewHolder viewHolder, final String s) {
+                if (mDates != null && mDates.size() != 0){
+                    int i = 0;
+                    viewHolder.setTextView(R.id.tv,mDates.get(i++));
+
+                }
+            }
+        };
+        commonAdapter.setOnItemClickListener(new RecyclerViewvViewCommonAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(final View view, final int position) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        commonAdapter.setOnItemLongClickListener(new RecyclerViewvViewCommonAdapter.onItemLongClickListener() {
+            @Override
+            public void onItemLongClickListener(final View view, final int position) {
+                Toast.makeText(MainActivity.this, "LongClick", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mRecyclerView.setAdapter(commonAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setNestedScrollingEnabled(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
+}
---
+++ app/src/main/java/com/example/aolei/recyclerviewadapter/RecyclerViewHolder.java
@@ -0,0 +1,95 @@
+package com.example.aolei.recyclerviewadapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by aolei on 2016/7/20.
+ */
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder
+{
+    private SparseArray<View> mViews;
+    private View mConvertView;
+    private Context mContext;
+    private int mPosition;
+    private int mLayoutId;
+
+    /**
+     * 构造函数
+     * @param context
+     * @param parent
+     * @param itemView
+     */
+    public RecyclerViewHolder(Context context, final View itemView,ViewGroup parent) {
+        super(itemView);
+        mConvertView = itemView;
+        mContext = context;
+        mViews = new SparseArray<View>();
+    }
+
+    /**
+     * 通过获取自定义布局，
+     * @param context
+     * @param parent
+     * @param layoutId
+     * @return
+     */
+    public static RecyclerViewHolder get(Context context,ViewGroup parent,int layoutId){
+        View itemView = LayoutInflater.from(context).inflate(layoutId,parent,false);
+        RecyclerViewHolder holder = new RecyclerViewHolder(context,itemView,parent);
+        return holder;
+    }
+
+    /**
+     *获取View
+     * @return
+     */
+    public View getConvertView(){
+        return mConvertView;
+    }
+
+    /**
+     * 通过viewId获取控件
+     * @param viewId
+     * @param <T>
+     * @return
+     */
+    public <T extends View> T getView(int viewId){
+        View view = mViews.get(viewId);
+        if (view == null){
+            view = mConvertView.findViewById(viewId);
+            mViews.put(viewId,view);
+        }
+        return (T) view;
+    }
+
+    /**
+     * 给TextView填充值
+     * @param viewId
+     * @param text
+     * @return
+     */
+    public RecyclerViewHolder setTextView(int viewId,String text){
+        TextView mTextView = getView(viewId);
+        mTextView.setText(text);
+        return this;
+    }
+
+    /**
+     * 设置监听事件
+     * @param viewId
+     * @param listener
+     * @return
+     */
+    public RecyclerViewHolder setOnClickListener(int viewId,View.OnClickListener listener){
+        View view = getView(viewId);
+        view.setOnClickListener(listener);
+        return this;
+    }
+}
